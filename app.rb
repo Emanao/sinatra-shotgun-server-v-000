005063143c9deb50d9b5c4281522f45e27334d1b
Eva Navarro <emanao@me.com>
2020-02-22 21:02:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Started my server using shotgun!"+" Eva"
+    "Started my server using shotgun!"+" Eva Navarro"
   end
 
 end
